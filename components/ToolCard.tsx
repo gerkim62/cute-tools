b193fe811feb31ffa0e4f60b1328b15781fb1eaa
Gerison Kimathi Muriungi <72649107+gerkim62@users.noreply.github.com>
2023-10-15 16:37:46 +0000

    made toolcard cuter

--- components/ToolCard.tsx
+++ components/ToolCard.tsx
@@ -6,14 +6,15 @@ type ToolCardProps = {
   link: string;
 };
 
-export default function ToolCard({ title, description, link }:ToolCardProps) {
+export default function ToolCard({ title, description, link }: ToolCardProps) {
   return (
-    <div className="bg-white p-6 rounded-lg text-left">
-      <h2 className="text-pink-500 text-xl font-semibold mb-4">{title}</h2>
+    <div className="bg-pink-100 p-6 rounded-lg shadow-lg text-left">
+      <h2 className="text-pink-600 text-2xl font-semibold mb-4">{title}</h2>
       <p className="text-gray-700">{description}</p>
+
       <Link
-        className="text-pink-600 hover:underline hover:text-pink-700 font-semibold transition duration-300 underline"
         href={link}
+        className="text-pink-600 hover:underline hover:text-pink-700 font-semibold transition duration-300"
       >
         Launch {title}
       </Link>
