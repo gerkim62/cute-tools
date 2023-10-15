978ac63c968046f61429761cb5ab1dfc21429500
Gerison Kimathi Muriungi <72649107+gerkim62@users.noreply.github.com>
2023-10-15 18:20:14 +0000

    fixed tool cards

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 text-center sm:text-left w-[90%] mb-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8 text-center sm:text-left w-[90%] mb-4">
         {toolsData.map((tool, index) => (
           <ToolCard
             key={index}
